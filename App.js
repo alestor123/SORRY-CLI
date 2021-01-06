5de6b75361faf5c0b9fb83d9e712021f252f3bdd
alestor123 <alestoraldous@gmail.com>
2021-01-06 17:26:31 +0530

    added main code

---
+++ App.js
@@ -0,0 +1,7 @@
+var axios = require('axios');
+
+(async () => {
+var res = await axios.get('https://apologygenerator.com/assets/apology.json')
+console.log(res.data.introduction[random(res.data.introduction)] + res.data.sadness[random(res.data.sadness)] + res.data.explanation[random(res.data.explanation)] + res.data.conclusion[random(res.data.conclusion)])  
+})();
+var random = op =>  Math.floor(Math.random()*op.length);
